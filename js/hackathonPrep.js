0651dc0907dbd0b6407a980255a95128c1f61b40
jordannp1997 <jordanpowell1997@yahoo.com>
2016-09-14 02:01:34 -0400

    VERY IMPORTANT HACKATHON PREP

---
+++ js/hackathonPrep.js
@@ -0,0 +1,37 @@
+function hackathon()
+{
+	console.log(bring());
+}
+
+function bring()
+{
+
+	var bringArray = []; 
+
+	bringArray.push("27 Inch Monitor");
+
+	bringArray.push("Yamaha Monitor Speaker"); /* For smooth jams while coding */
+
+	bringArray.push("My beast of a computer");
+
+	bringArray.push("G Fuel");
+
+	bringArray.push("Water");
+
+	bringArray.push("Blankets"); /* Incase we sleep in the car */
+
+	bringArray.push("More blankets"); /* To wrap the electronics */
+
+	bringArray.push(shmackables());
+
+	bringArray.push("$200");
+
+	return bringArray;
+
+}
+
+function shmackables()
+{
+	var snacks = ["Fruit Snacks", "Doritos", "Pop Tarts", "Chic Peas"];
+	return snacks;
+}
