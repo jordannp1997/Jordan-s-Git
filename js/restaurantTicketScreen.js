d8c64b680b59ad913e41a6a067429e0de85305af
Jordan <jordanpowell1997@yahoo.com>
2017-01-11 11:07:21 -0500

    Add files via upload

---
+++ js/restaurantTicketScreen.js
@@ -0,0 +1,58 @@
+var menuScreen = [];
+var orderIn = [];
+var orderComplete;
+var orderNum = 0;
+var isNewOrder = undefined;
+
+function newOrder(drink, meal, side, dessert)
+{
+	this.drink = drink;
+	this.meal = meal;
+	this.side = side;
+	this.dessert = dessert;
+
+	isNewOrder = true;
+
+	if(this.drink != undefined)
+	{
+		menuScreen.push(drink);
+	}
+
+	if(this.meal != undefined)
+	{
+		menuScreen.push(meal);
+	}
+
+	if(this.side != undefined)
+	{
+		menuScreen.push(side);
+	}
+
+	if(this.dessert != undefined)
+	{
+		menuScreen.push(dessert);
+	}
+
+	cycleOrderNum();
+}
+
+function cycleOrderNum()
+{
+	if(isNewOrder = true)
+	{
+		orderNum++;
+	}
+	else
+	{
+		// Do nothing
+	}
+
+	viewOrder();
+}
+
+function viewOrder()
+{
+	console.log("Order #" + orderNum + ": " + menuScreen);
+}
+
+newOrder("Coke", "Big Mac", "Fries", "Shake");
